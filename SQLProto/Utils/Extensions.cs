c6041c2af7858535c42229c1da2a486e0d689ace
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2020-10-09 22:10:15 +0200

    restapi start

--- SQLProto/Utils/Extensions.cs
+++ SQLProto/Utils/Extensions.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SQLProto.Utils
 {
@@ -17,6 +21,55 @@ namespace SQLProto.Utils
                 return toAdd;
             else
                 return obj.Concat(toAdd);
+        }
+
+        public static string ReadUtf8Line(this Stream stream)
+        {
+            var bytes = new List<byte>();
+            while (true)
+            {
+                var readed = stream.ReadByte();
+                if (readed == -1 || readed == '\n')
+                {
+                    break;
+                }
+
+                bytes.Add((byte) readed);
+            }
+
+            if (bytes.Any() && bytes.Last() == '\r')
+            {
+                return Encoding.UTF8.GetString(bytes.Take(bytes.Count - 1).ToArray());
+            }
+            else
+            {
+                return Encoding.UTF8.GetString(bytes.ToArray());
+            }
+        }
+
+        public static async Task<string> ReadUtf8LineAsync(this Stream stream)
+        {
+            var bytes = new List<byte>();
+            var readedByte = new byte[1];
+            while (true)
+            {
+                var readed = await stream.ReadAsync(readedByte, 0, 1);
+                if (readed == 0 || readedByte[0] == '\n')
+                {
+                    break;
+                }
+
+                bytes.Add(readedByte[0]);
+            }
+
+            if (bytes.Last() == '\r')
+            {
+                return Encoding.UTF8.GetString(bytes.Take(bytes.Count - 1).ToArray());
+            }
+            else
+            {
+                return Encoding.UTF8.GetString(bytes.ToArray());
+            }
         }
     }
 }
